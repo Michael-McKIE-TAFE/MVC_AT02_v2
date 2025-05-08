b3cd09b6de76ae3f3c918c3e2fee6fec68a3d1a3
30086755 <30086755@tafe.wa.edu.au>
2025-05-08 16:51:50 +0800

    Minor change to APU versioning

--- McKIESales.API/Program.cs
+++ McKIESales.API/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddApiVersioning(options => {
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new ApiVersion(1, 0);
     options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+    //options.ApiVersionReader = new QueryStringApiVersionReader("api-version");
 }).AddMvc().AddApiExplorer(options => {
     options.GroupNameFormat = "'v'VVV";
     options.SubstituteApiVersionInUrl = true;
