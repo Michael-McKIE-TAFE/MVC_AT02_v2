1d3ed7924eb175dbb06dcc8916f0027f7724ab21
Michael McKIE <30086755@tafe.wa.edu.au>
2025-05-15 09:52:11 +0800

    Added CORS

--- McKIESales.API/Program.cs
+++ McKIESales.API/Program.cs
@@ -45,6 +45,10 @@ builder.Services.Configure<RouteOptions>(options => {
     options.ConstraintMap.Add("apiVersion", typeof(ApiVersionRouteConstraint));
 });
 
+builder.Services.AddCors(options => { 
+        options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 var apiProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
