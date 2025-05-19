2494e7aa1900399fa1f1fefbe627383c71767e17
Michael McKIE <30086755@tafe.wa.edu.au>
2025-05-19 11:59:22 +0800

    Updated Comment

--- McKIESales.WEB/Models/RegisterViewModel.cs
+++ McKIESales.WEB/Models/RegisterViewModel.cs
@@ -2,14 +2,12 @@
 
 namespace McKIESales.WEB.Models {
     /// <summary>
-    /// The `RegisterViewModel` class is a model used to capture and validate 
-    /// the data inputted by the user during registration. It contains three 
-    /// properties: `Username`, `Email`, and `Password`. Each property is 
-    /// decorated with validation attributes to ensure proper data entry. 
-    /// `Username` and `Email` are required fields, and `Email` is further 
-    /// validated to check if the input is a valid email address. The `Password` 
-    /// field is required and its data type is set to `Password`, which is 
-    /// typically used for masking passwords during input.
+    /// This model used to capture and validate the data inputted by the user during registration. 
+    /// It contains three properties: Username Email Password 
+    /// Each property is decorated with validation attributes to ensure proper data entry. 
+    /// Username and Email are required fields, and Email is further validated to check if the 
+    /// input is a valid email address. The Password field is required, and its data type is set to 
+    /// Password, which is typically used for masking passwords during input. 
     /// </summary>
     public class RegisterViewModel {
         [Required]
